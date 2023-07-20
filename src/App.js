c05b2771b0f4c39e4b49eeaa05e2cc0d8ece340a
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-20 21:33:48 +0300

    Counter useState demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+    const [count, setCount] = useState(2)
+
+    function decrementCount () {
+        setCount(prevCount => prevCount - 1)
+
+    }
+    function incrementCount () {
+        setCount(count + 1)
+
+    }
   return (
-    <div className="App">
-     
+    <div>
+     <button onClick = {decrementCount}>-</button>
+     <span>{count}</span>
+     <button onClick = {incrementCount}>+</button>
     </div>
   );
 }
